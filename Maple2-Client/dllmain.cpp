4ce5d3669ae0617df48ec6d2ee1b1ff9635dfcff
Kang Jinsu <jinsu8527@gmail.com>
2024-05-26 03:55:02 +0900

    dllmain.cpp edit (The X86 and X64 reads maple2.ini on the route of the client)

--- Maple2-Client/dllmain.cpp
+++ Maple2-Client/dllmain.cpp
@@ -36,11 +36,21 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved) {
       }
 
       // Load config file to global variables.
+#ifndef _WIN64 // Maple2.ini is the root path. If this client is x64, the client is in the x64 folder, so read the file in the upper path.
       if (!config::Load("maple2.ini")) {
         ShowWindow(GetConsoleWindow(), SW_RESTORE);
         MessageBoxA(NULL, "Failed to load config.", "Error", MB_ICONERROR | MB_OK);
         return FALSE;
       }
+#else
+      if (!config::Load("../maple2.ini")) {
+        ShowWindow(GetConsoleWindow(), SW_RESTORE);
+        MessageBoxA(NULL, "Failed to load config.", "Error", MB_ICONERROR | MB_OK);
+        return FALSE;
+      }
+#endif // !_WIN64
+
+
 
       // Redirect the process with custom args.
       if (!hook::RedirectProcess()) {
